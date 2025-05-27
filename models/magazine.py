18ba0d535fae5e6e07c23c24c2feb3f20111b90f
Kelly Koome <koomekelly7@gmail.com>
2025-05-27 20:00:07 +0300

    Refactor database structure and models; remove unused files and implement new schema

---
+++ models/magazine.py
@@ -0,0 +1,121 @@
+from lib.db.connection import get_connection
+
+class Magazine:
+    def __init__(self, id=None, name=None, category=None):
+        self.id = id
+        self.name = name
+        self.category = category
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        if self.id:
+            cursor.execute("UPDATE magazines SET name = ?, category = ? WHERE id = ?", 
+                          (self.name, self.category, self.id))
+        else:
+            cursor.execute("INSERT INTO magazines (name, category) VALUES (?, ?)", 
+                         (self.name, self.category))
+            self.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+    def delete(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM magazines WHERE id = ?", (self.id,))
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM magazines WHERE id = ?", (id,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(id=row['id'], name=row['name'], category=row['category'])
+        return None
+
+    @classmethod
+    def find_by_name(cls, name):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM magazines WHERE name = ?", (name,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(id=row['id'], name=row['name'], category=row['category'])
+        return None
+
+    @classmethod
+    def find_by_category(cls, category):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM magazines WHERE category = ?", (category,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [cls(id=row['id'], name=row['name'], category=row['category']) for row in rows]
+
+    def articles(self):
+        from lib.models.article import Article
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE magazine_id = ?", (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Article(id=row['id'], title=row['title'], author_id=row['author_id'], magazine_id=row['magazine_id']) for row in rows]
+
+    def contributors(self):
+        from lib.models.author import Author
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT a.* FROM authors a
+            JOIN articles ar ON a.id = ar.author_id
+            WHERE ar.magazine_id = ?
+        """, (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Author(id=row['id'], name=row['name']) for row in rows]
+
+    def article_titles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT title FROM articles WHERE magazine_id = ?", (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [row['title'] for row in rows]
+
+    def contributing_authors(self):
+        from lib.models.author import Author
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT a.*, COUNT(ar.id) as article_count
+            FROM authors a
+            JOIN articles ar ON a.id = ar.author_id
+            WHERE ar.magazine_id = ?
+            GROUP BY a.id
+            HAVING article_count > 2
+        """, (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Author(id=row['id'], name=row['name']) for row in rows]
+
+    @classmethod
+    def magazines_with_multiple_authors(cls):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT m.* FROM magazines m
+            JOIN (
+                SELECT magazine_id, COUNT(DISTINCT author_id) as author_count
+                FROM articles
+                GROUP BY magazine_id
+                HAVING author_count >= 2
+            ) a ON m.id = a.magazine_id
+        """)
+        rows = cursor.fetchall()
+        conn.close()
+        return [cls(id=row['id'], name=row['name'], category=row['category']) for row in rows]
